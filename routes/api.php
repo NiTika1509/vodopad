73517d7a03d945a6b5f9cedd885f81cb98e8df84
Tika Zakharov <nikit@DESKTOP-KQGL45E.localdomain>
2022-11-08 01:19:55 +0300

    Nikita 08.11.2022

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::prefix('admin')->group(function (){
+
+    Route::get('/',function (){
+
+
+    });
+
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
